c6048460977589b97368ff98ae95d772e53ebb79
Healm <116085297+Agent-Healm@users.noreply.github.com>
2026-04-24 07:01:04 +0700

    combine container creation with app runner

--- DBeaver/dbeaver-launcher.zsh
+++ DBeaver/dbeaver-launcher.zsh
@@ -1,10 +1,24 @@
 container_name="cloudbeaver"
 
+start_container() {
+        docker run -d \
+                -p 8978:8978 \
+                --name $container_name \
+                --restart=unless-stopped \
+                -v cloudbeaver_data:/opt/cloudbeaver/workspace \
+                dbeaver/cloudbeaver:26.0.1
+}
+
+docker ps -a --format '{{.Names}}' \
+        | grep $container_name \
+        || (echo no container exist \
+        && start_container)
+
 docker ps --format '{{.Names}}' \
-        | grep -q "$container_name" \
-        || (echo no container \
+        | grep "$container_name" \
+        || (echo container inactive \
         && docker start $container_name \
-        && sleep 1.5) \
+        && sleep 2.5) \
         && xdg-open http://localhost:8978
 
 sleep 1200 \
