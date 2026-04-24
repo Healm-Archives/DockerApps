36e0a6f26c48641c377eab3b9a64e76d4f6b25b9
Healm <116085297+Agent-Healm@users.noreply.github.com>
2026-04-24 07:43:18 +0700

    added if for handling container state

--- DBeaver/dbeaver-launcher.zsh
+++ DBeaver/dbeaver-launcher.zsh
@@ -1,6 +1,7 @@
 container_name="cloudbeaver"
+# container_status=""
 
-start_container() {
+run_container() {
         docker run -d \
                 -p 8978:8978 \
                 --name $container_name \
@@ -9,18 +10,39 @@ start_container() {
                 dbeaver/cloudbeaver:26.0.1
 }
 
-docker ps -a --format '{{.Names}}' \
-        | grep $container_name \
-        || (echo no container exist \
-        && start_container)
+# docker ps -a --filter "name=$container_name" \
+# container_status() {
+status=$(docker ps -a --filter "name=cloudbeaver" \
+        --format '{{.Status}}' \
+        | awk '{print $1}' \
+        | grep -E 'Up|Exited')
+# }
 
-docker ps --format '{{.Names}}' \
-        | grep "$container_name" \
-        || (echo container inactive \
-        && docker start $container_name \
-        && sleep 2.5) \
-        && xdg-open http://localhost:8978
+if [ -z "$status" ]; then
+        echo "container doesnt exist"
+        run_container
+# elif [[ $status = "Up" ]]; then
+else
+        # echo "container exist with status: $status"
+        echo "container inactive" \
+                && docker start $container_name
+fi
 
-sleep 1200 \
-&& docker stop $container_name
+sleep 2.5 \
+&& xdg-open http://localhost:8978
+
+# docker ps -a --format '{{.Names}}' \
+#         | grep $container_name \
+#         || (echo no container exist \
+#         && run_container)
+
+# docker ps --format '{{.Names}}' \
+#         | grep "$container_name" \
+#         || (echo container inactive \
+#         && docker start $container_name \
+#         && sleep 2.5) \
+#         && xdg-open http://localhost:8978
+
+# sleep 1200 \
+# && docker stop $container_name
 
